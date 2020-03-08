b28cc4cbe603ef53922f04094648e4bfd5f67b98
aakash123 khurana <=>
2020-03-08 09:23:19 +0530

    Changes

--- src/main/java/websocket/config/Handler.java
+++ src/main/java/websocket/config/Handler.java
@@ -1,5 +1,6 @@
 package websocket.config;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -8,12 +9,9 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Handler extends TextWebSocketHandler {
+public class Handler extends TextWebSocketHandler {as
 
     private List<WebSocketSession> list = new ArrayList<>();
-
-
-
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
@@ -23,6 +21,8 @@ public class Handler extends TextWebSocketHandler {
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        System.out.println(message.getPayload());
+        JSONObject jsonObject  = new JSONPObject(message.getPayload());
         for(WebSocketSession s : list){
                 s.sendMessage(message);
         }

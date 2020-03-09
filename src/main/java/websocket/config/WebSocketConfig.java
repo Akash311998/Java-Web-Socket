3ca8d58e48bcacedc48c248e78411da78135c65a
aakash123 khurana <=>
2020-03-09 16:07:16 +0530

    commit

--- src/main/java/websocket/config/WebSocketConfig.java
+++ src/main/java/websocket/config/WebSocketConfig.java
@@ -6,24 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-import websocket.config.*;
+import websocket.config.Handler;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Autowired
-    private Handler hander;
-
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(hander,"/websocket").setAllowedOrigins("*");
-    }
-
-    @Bean
-    public Handler handlerBean(){
-        return new Handler();
+        webSocketHandlerRegistry.addHandler(new Handler(),"/websocket").setAllowedOrigins("*");
     }
 
 
